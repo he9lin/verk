0702e8d6e5c5ffb779f98e11486338631e000b39
Lin He <he9lin@gmail.com>
2023-03-10 20:19:53 -0800

    Refactor opts as env

--- lib/verk/schedule_manager.ex
+++ lib/verk/schedule_manager.ex
@@ -29,7 +29,8 @@ defmodule Verk.ScheduleManager do
   Connect to redis and timeout with the `poll_interval`.
   """
   def init(_) do
-    {:ok, redis} = Redix.start_link(Confex.get_env(:verk, :redis_url), socket_opts: [verify: :verify_none])
+    start_opts = Confex.get_env(:verk, :redis_start_opts, [])
+    {:ok, redis} = Redix.start_link(Confex.get_env(:verk, :redis_url), start_opts)
     Verk.Scripts.load(redis)
 
     state = %State{redis: redis}
